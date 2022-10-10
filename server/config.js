8309f60a1b8710379003066065b4dc955265ca56
anoop <yedullaanoopreddy22@gmail.com>
2022-10-10 16:12:01 +0530

    version-2

--- server/config.js
+++ server/config.js
@@ -1,7 +1,6 @@
+require('dotenv').config()
+
 module.exports={
-    DATABASE_CONNECTION:"mongodb+srv://Anoop:Anoop2002@cluster0.quj2y.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET: "sdfgfhjdrhyrfhyrfthrfthfvhgrh",
-    CLOUD_NAME:"anoop23",
-    API_KEY:818667547189548,
-    API_SECRET:"kJ8-V7kOVzA48oXp_IpVzFUUei0"
+    DATABASE_CONNECTION:process.env.DATABASE_CONNECTION,
+    JWT_SECRET: process.env.JWT_SECRET,
 }
